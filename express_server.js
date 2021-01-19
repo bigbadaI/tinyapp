3a18ae88c03f868996d8e1868bfedd59abb5ede6
BigBadAI <flashmefusion@hotmail.ca>
2021-01-19 21:04:53 +0000

    Finally fixed the urls index to now post correctly and exprees_server to see the post and delete the corresponding urls

--- express_server.js
+++ express_server.js
@@ -38,6 +38,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const shortUrl = templateVars.shortURL;
+  delete urlDatabase[shortUrl],
+  res.redirect("/urls");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   
@@ -50,11 +56,6 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longUrl);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  delete urlDatabase[templateVars],
-  res.redirect("/urls");
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
