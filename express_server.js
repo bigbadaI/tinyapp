b0e183ea5462a046c4a6d298bcacd42eeb9008b2
BigBadAI <flashmefusion@hotmail.ca>
2021-01-21 21:37:30 +0000

    Removed cookie parser from files since we have moved on to cookie session

--- express_server.js
+++ express_server.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const cookieSession = require("cookie-session");
 
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(cookieSession({
